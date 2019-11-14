8bc2f4643439ef1b9bc4351733be1d80c72ed016
ProgrammnieInzheneri <55140067+ProgrammnieInzheneri@users.noreply.github.com>
2019-11-14 16:48:30 +0400

    HTML

--- login.php
+++ login.php
@@ -8,11 +8,16 @@ if (isAdmin())
 else
 {
     $c =
-        '<form class="col-md-3" method="POST">
+        '<div class="container">
+			<div class="row justify-content-center">
+			<div class="col-3">
+			<h4 class="text-center">Вход</h4>
             <input name="login" class="form-group form-control" type="text" placeholder="Логин"/>
             <input name="password" class="form-group form-control" type="password" placeholder="Пароль"/>
-            <input class="form-group form-control btn-primary" type="submit" value="Войти"/>
-        </form>';
+            <input class="form-group form-control btn-info" type="submit" value="Зайдитэ"/>
+        </div>
+		</div>
+		</div>';
     $param['title'] = 'Login | '.$config['site_title'];
     $param['content'] = $c;
     template($param);
--- register.php
+++ register.php
@@ -8,15 +8,25 @@ if (isAdmin())
 else
 {
     $c =
-        '<form class="col-md-3" method="POST">
+        '<div class="container">
+			<div class="row justify-content-center">
+			<div class="col-3">
+			<h4 class="text-center">Регистрация</h4>
+			<h6 class="text-center">
+			<small class="text-muted">
+			Поля со звездочкой обязательны
+			</small>
+			</h6>
             <input name="name" class="form-group form-control" type="text" placeholder="Имя*"/>
             <input name="login" class="form-group form-control" type="text" placeholder="Логин*"/>
             <input name="password" class="form-group form-control" type="password" placeholder="Пароль*"/>
             <input name="email" class="form-group form-control" type="email" placeholder="Почта*"/>
             <input name="phone" class="form-group form-control" type="tel" placeholder="Телефон"/>
             <input name="adress" class="form-group form-control" type="text" placeholder="Адрес"/>
-            <input class="form-group form-control btn-primary" type="submit" value="Зарегацца"/>
-        </form>';
+            <input class="form-group form-control btn-info" type="submit" value="Зарегацца"/>
+        </div>
+		</div>
+		</div>';
     $param['title'] = 'Регистрация | '.$config['site_title'];
     $param['content'] = $c;
     template($param);
